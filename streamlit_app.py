f3972119f6807229462be2409243541d156e9b4a
Amri Hidayat <amri.hidayat@iproperty.com.my>
2023-11-01 11:13:08 +0800

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,8 @@ import presidio_analyzer
 import presidio_anonymizer
 import spacy
 import spacy_streamlit
+import time
+
 
 from presidio_anonymizer import AnonymizerEngine, DeanonymizeEngine
 from presidio_anonymizer.entities import RecognizerResult, OperatorResult, OperatorConfig
@@ -17,6 +19,8 @@ engine = AnonymizerEngine()
 # Invoke the anonymize function with the text,
 # analyzer results (potentially coming from presidio-analyzer)
 # and an 'encrypt' operator to get an encrypted anonymization output:
+start_time = time.time()
+
 anonymize_result = engine.anonymize(
     text="My name is James Bond",
     analyzer_results=[
@@ -26,4 +30,18 @@ anonymize_result = engine.anonymize(
 )
 
 anonymize_result
+end_time = time.time()
+execution_time = (end_time - start_time) * 1000  # Convert to milliseconds
+
+execution_time
+
+start_time = time.time()
+
+encrypted_entity_value = anonymize_result.items[0].text
+
+# Restore the original entity value
+Decrypt().operate(text=encrypted_entity_value, params={"key": crypto_key})
+end_time = time.time()
+execution_time = (end_time - start_time) * 1000  # Convert to milliseconds
+
     
