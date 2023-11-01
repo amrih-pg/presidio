414d551d267b0caccfc4e42079236794ed4abcdd
Amri Hidayat <amri.hidayat@iproperty.com.my>
2023-11-01 10:48:12 +0800

    Install presidio dependencies

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,4 +2,19 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
+# Install Presidio dependencies
+st.write("Installing Presidio dependencies...")
+st.system("pip install presidio_analyzer presidio_anonymizer")
+st.system("python -m spacy download en_core_web_lg")
+
 st.title('Presidio testing')
+
+# Import other necessary libraries
+import presidio_analyzer
+import presidio_anonymizer
+import spacy
+
+
+# Run the Streamlit app
+if __name__ == "__main__":
+    st.run_app()
