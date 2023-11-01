9525cd505e25777cdacb6547e0e1728c729ab569
Amri Hidayat <amri.hidayat@iproperty.com.my>
2023-11-01 10:53:07 +0800

    Update

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,8 +10,14 @@ st.title('Presidio testing')
 import presidio_analyzer
 import presidio_anonymizer
 import spacy
+import spacy_streamlit
+
 
 
 # Run the Streamlit app
 if __name__ == "__main__":
     st.run_app()
+
+    models = ["en_core_web_sm", "en_core_web_md"]
+    default_text = "Sundar Pichai is the CEO of Google."
+    spacy_streamlit.visualize(models, default_text)
