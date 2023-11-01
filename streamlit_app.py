e129ae7d07a1c902b69d1667f085b9d42299012c
Amri Hidayat <amri.hidayat@iproperty.com.my>
2023-11-01 11:00:20 +0800

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,6 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-
-# Install Presidio dependencies
-st.title('Presidio testing')
-
-# Import other necessary libraries
 import presidio_analyzer
 import presidio_anonymizer
 import spacy
